cdc4a318f11fb255471e0406268d40c3a8fd6e58
Ahmet <scienceworker34@gmail.com>
2021-04-29 18:22:33 +0300

    add models

--- src/quiz/admin.py
+++ src/quiz/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+import nested_admin
+from .models import Question, Quiz, Category, Answer
 
-# Register your models here.
+class AnswerInline(nested_admin.NestedTabularInline):
+    model = Answer
+
+class QuestionInline(nested_admin.NestedTabularInline):
+    model = Question
+    inlines = [AnswerInline]
+    
+class QuizAdmin(nested_admin.NestedModelAdmin):
+    model = Quiz
+    inlines = [QuestionInline]
+    
+    
+admin.site.register(Category)
+admin.site.register(Quiz,QuizAdmin)
+admin.site.register(Question)
+admin.site.register(Answer)
